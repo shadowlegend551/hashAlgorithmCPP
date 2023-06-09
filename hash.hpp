232bd78009d4c87973f9f44b5abf17b5b494b7f6
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2023-06-09 13:47:45 +0300

    Initial commit.

--- hash.hpp
+++ hash.hpp
@@ -4,8 +4,10 @@
 #include <vector>
 #include <string>
 
+// Converts a string to the lower case ascii values of said string. e.g. "ABC" becomes "979899"
 std::string ascii_encode(std::string input_string);
 
+// Calculates a value based on strings in the strings-vector with the maximum value being limit.
 int hash(std::vector<std::string> strings, int limit);
 
 #endif
